8252445934741c606db7203ad755dd366d75e362
Lucius Vinicius <luciusviniciusf@ua.pt>
2023-11-18 16:01:47 +0000

    Better description of visualizer functions

--- visualizer.py
+++ visualizer.py
@@ -10,15 +10,19 @@ class Visualizer():
     background_color: tuple[int, int, int] = (255, 255, 255)
     
     def start(self):
+        """Setups the window for the visualizer."""
         self.screen = pygame.display.set_mode(self.window_size)
     
-    def play(self):
+    def update(self):
+        """Updates the window with the new color."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
         pygame.display.flip()
         self.screen.fill(self.background_color)
     
-    def change_to_color(self, color: tuple[int, int, int]):
+    def change_to_color(self, color: tuple[int, int, int], fade_time: float = 0.0):
+        """Changes and updates the visualizer background color to the given one.
+        TODO: Add a smooth effect."""
         self.background_color = color
-        self.play()
+        self.update()
